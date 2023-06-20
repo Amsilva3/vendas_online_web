f265392af025baaa942e3709af5187bf67c6b45d
Andre Miranda da Silva <amsilva3@gmail.com>
2023-06-20 13:39:28 +0200

    create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackgroundImage src="./Background.png" />
+    </div>
+  );
+};
+
+export default LoginScreen;
