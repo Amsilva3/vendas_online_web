df24b1fdfe5f22f43b773734cb8f378e14fae74b
Andre Miranda da Silva <amsilva3@gmail.com>
2023-07-08 14:54:44 +0200

    insert verify user logged in

--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -13,7 +13,7 @@ import { useGlobalContext } from './useGlobalContext';
 export const useRequests = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const { setNotification } = useGlobalContext();
+  const { setNotification, setUser } = useGlobalContext();
 
   const getRequest = async (url: string) => {
     setLoading(true);
@@ -46,6 +46,7 @@ export const useRequests = () => {
     setLoading(true);
     await ConnectionAPIPost<AuthType>(URL_AUTH, body)
       .then((result) => {
+        setUser(result.user);
         setAuthorizationToken(result.accessToken);
         navigate(ProductRoutesEnum.PRODUCT);
         return result;
