7802f7096809c18274cfdd9eba5a413cb85cdf31
Andre Miranda da Silva <amsilva3@gmail.com>
2023-07-08 15:53:37 +0200

    insert page not found and remove verify user in firstScreen

--- src/modules/firstScreen/screens/FirstScreen.tsx
+++ src/modules/firstScreen/screens/FirstScreen.tsx
@@ -2,35 +2,17 @@ import { Spin } from 'antd';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { URL_USER } from '../../../shared/constants/urls';
-import {
-  getAuthorizationToken,
-  unsetAuthorizationToken,
-} from '../../../shared/functions/connection/auth';
-import { ConnectionAPIGet } from '../../../shared/functions/connection/connectionAPI';
-import { LoginRoutesEnum } from '../../login/routes';
+import { useGlobalContext } from '../../../shared/hooks/useGlobalContext';
 import { ProductRoutesEnum } from '../../product/routes';
 
 const FirstScreen = () => {
+  const { user } = useGlobalContext();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const verifyToken = async () => {
-      const token = getAuthorizationToken();
-      if (token) {
-        await ConnectionAPIGet(URL_USER)
-          .then(() => {
-            navigate(ProductRoutesEnum.PRODUCT);
-          })
-          .catch(() => {
-            unsetAuthorizationToken();
-            navigate(LoginRoutesEnum.LOGIN);
-          });
-      } else {
-        navigate(LoginRoutesEnum.LOGIN);
-      }
-    };
-    verifyToken();
+    if (user) {
+      navigate(ProductRoutesEnum.PRODUCT);
+    }
   }, []);
 
   return <Spin />;
--- src/modules/firstScreen/screens/routes.tsx
+++ src/modules/firstScreen/screens/routes.tsx
@@ -1,11 +1,12 @@
 import { RouteObject } from 'react-router-dom';
 
 import FirstScreen from '..';
+import PageNotFound from './PageNotFound';
 
 export const firstScreenRoutes: RouteObject[] = [
   {
     path: '/',
     element: <FirstScreen />,
-    errorElement: <div>Pagina não encontrada</div>,
+    errorElement: <PageNotFound />,
   },
 ];
