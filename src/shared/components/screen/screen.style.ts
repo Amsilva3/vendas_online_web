a4238291a1667e1ed9927d769cbbf96e3fc9a00c
Andre Miranda da Silva <amsilva3@gmail.com>
2023-10-10 12:30:04 +0200

    feat: insert menu in Screen

--- src/shared/components/screen/screen.style.ts
+++ src/shared/components/screen/screen.style.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const ScreenContainer = styled.div`
+  background-color: white;
   padding: 32px;
+  margin: 32px 32px 32px 220px;
+  width: calc(100%- 368px);
 `;
