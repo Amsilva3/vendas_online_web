4cce5383fb9383ee5194a8de24cb0df7cc807283
Andre Miranda da Silva <amsilva3@gmail.com>
2023-10-11 12:21:49 +0200

    feat: refactor location.href

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,11 +1,12 @@
-import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { Button, Modal, Space } from 'antd';
-import React, { useState } from 'react';
+import { Modal } from 'antd';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { logout } from '../../functions/connection/auth';
 import { HeaderContainer, LogoExit } from './header.style';
 
 const Header = () => {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
 
   const showModal = () => {
@@ -20,7 +21,7 @@ const Header = () => {
       <Modal
         title="Atenção"
         open={open}
-        onOk={logout}
+        onOk={() => logout(navigate)}
         onCancel={hideModal}
         okText="Sim"
         cancelText="Cancelar"
