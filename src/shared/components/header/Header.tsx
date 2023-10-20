6ee1d24ca558660b26664f5d77b8a62ca57900a0
Andre Miranda da Silva <amsilva3@gmail.com>
2023-10-20 17:46:10 +0200

    feat:create unit test in Header

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { logout } from '../../functions/connection/auth';
+import { HeaderTestIdEnum } from './__tests__/headerTestIdEnum';
 import { HeaderContainer, LogoExit } from './header.style';
 
 const Header = () => {
@@ -16,9 +17,11 @@ const Header = () => {
   const hideModal = () => {
     setOpen(false);
   };
+
   return (
     <>
       <Modal
+        data-testid={HeaderTestIdEnum.HEADER_MODAL}
         title="Atenção"
         open={open}
         onOk={() => logout(navigate)}
@@ -26,12 +29,13 @@ const Header = () => {
         okText="Sim"
         cancelText="Cancelar"
       >
-        <p>Tem certeza que deseja sair?</p>
+        <p data-testid={HeaderTestIdEnum.HEADER_MODAL_P}>Tem certeza que deseja sair?</p>
       </Modal>
-      <HeaderContainer>
-        <LogoExit onClick={showModal} />
+      <HeaderContainer data-testid={HeaderTestIdEnum.HEADER_CONTAINER}>
+        <LogoExit data-testid={HeaderTestIdEnum.HEADER_LOGO} onClick={showModal} />
       </HeaderContainer>
     </>
   );
 };
+
 export default Header;
