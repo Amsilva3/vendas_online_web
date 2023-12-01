18b92b9bd74c25a613fadfdd27b600edf255ae11
Andre Miranda da Silva <amsilva3@gmail.com>
2023-12-01 18:21:06 +0100

    feat: create user Screen

--- src/shared/components/menu/Menu.tsx
+++ src/shared/components/menu/Menu.tsx
@@ -14,6 +14,7 @@ import { CategoryRoutesEnum } from '../../../modules/category/routes';
 import { OrderRoutesEnum } from '../../../modules/orders/routes';
 import { ProductRoutesEnum } from '../../../modules/product/routes';
 import { ContainerLogoName, ContainerMenu, LogoMenu, NameCompany } from './menu.style';
+import { UserRoutesEnum } from '../../../modules/user/routes';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -71,6 +72,7 @@ const Menu = () => {
       key: 'user',
       label: 'Clientes',
       icon: <UserOutlined />,
+      onClick: () => navigate(UserRoutesEnum.USER),
     },
   ];
 
