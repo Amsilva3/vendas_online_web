4cb0ff7d4435716c4c865e01e969dc35d6c88dcf
Andre Miranda da Silva <amsilva3@gmail.com>
2024-02-19 21:02:53 +0100

    feat: insert unit test in Loading

---
+++ src/shared/components/loading/__tests__/Loading.spec.tsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+
+import Loading from '../Loading';
+
+export enum LoadingTestIds {
+  SPIN = 'SPIN',
+}
+
+describe('Test Loading', () => {
+  it('should render success', () => {
+    const { getByTestId } = render(<Loading data-testid={LoadingTestIds.SPIN} />);
+    expect(getByTestId(LoadingTestIds.SPIN)).toBeDefined();
+  });
+});
