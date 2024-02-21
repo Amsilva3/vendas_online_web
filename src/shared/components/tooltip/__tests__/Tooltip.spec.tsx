39ab889cdda3949a3c62f4401d51d8c41ab0b9e2
Andre Miranda da Silva <amsilva3@gmail.com>
2024-02-21 20:18:53 +0100

    feat: insert unit test in Tooltip

---
+++ src/shared/components/tooltip/__tests__/Tooltip.spec.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+
+import Tooltip from '../Tooltip';
+
+const mockChildren = 'mockChildren';
+const mockTitle = 'mockTitle';
+
+export enum TooltipTestIds {
+  CONTAINER_TOOLTIP = 'CONTAINER_TOOLTIP',
+  INFO_TOOLTIP = 'INFO_TOOLTIP',
+  ANTD_TOOLTIP = 'ANTD_TOOLTIP',
+}
+describe('Test Tooltip', () => {
+  it('should render success', () => {
+    const { getByTestId } = render(<Tooltip>{mockChildren}</Tooltip>);
+    expect(getByTestId(TooltipTestIds.CONTAINER_TOOLTIP)).toBeDefined();
+  });
+  it('should render children', () => {
+    const { getByText } = render(<Tooltip>{mockChildren}</Tooltip>);
+    expect(getByText(mockChildren)).toBeDefined();
+  });
+  it('should render antd tooltip', () => {
+    const { getByText, queryAllByTestId } = render(
+      <Tooltip title={mockTitle}>{mockChildren}</Tooltip>,
+    );
+    expect(getByText(mockChildren)).toBeDefined();
+    expect(queryAllByTestId(TooltipTestIds.CONTAINER_TOOLTIP).length).toEqual(0);
+  });
+});
